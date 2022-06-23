87d6100acf7a068f5441459ca4b23d6ffdf88e67
Salo <salomonco@gmail.com>
2022-06-23 16:46:15 -0500

    someone has connected / disconnected announcement works

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+    socket.broadcast.emit('chat message', "someone has connected");
+
+
+    console.log("a user connected");
   socket.on("disconnect", () => {
+    socket.broadcast.emit('chat message', 'someone has disconnected')
     console.log("user disconnected");
   });
+
   socket.on("chat message", (msg) => {
     io.emit('chat message', msg);
     console.log("message: " + msg);
   });
+
 });
-
-
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
